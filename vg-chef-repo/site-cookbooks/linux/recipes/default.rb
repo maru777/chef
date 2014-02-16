fa094c38b21b5fd3944ca103b06a28e7d8fb2383
U-DM-PC2\DM <DM@DM-PC2.(none)>
2014-02-17 00:24:25 +0900

    vgchef update

--- vg-chef-repo/site-cookbooks/linux/recipes/default.rb
+++ vg-chef-repo/site-cookbooks/linux/recipes/default.rb
@@ -9,8 +9,39 @@
 
 ## package install
 
-%w{ vim telnet }.each do |pack|
+%w{ vim telnet man }.each do |pack|
   package pack do
     action :install
   end
 end
+
+execute "nettools" do
+  command 'yum -y groupinstall "Networking Tools"'
+  action :run
+end
+
+file "/etc/profile" do
+  rc = Chef::Util::FileEdit.new(path)
+  my_profile = '[ -e /etc/my_profile ] && . /etc/my_profile'
+  rc.insert_line_if_no_match( my_profile, "#{my_profile}\n")
+  content rc.send(:contents).join
+  action :create
+end
+
+template "/etc/my_profile" do
+  source "env/my_profile.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  action :create
+end
+
+template "/usr/share/vim/vimrc" do
+  source "env/vimrc.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  action :create
+end
+
+
