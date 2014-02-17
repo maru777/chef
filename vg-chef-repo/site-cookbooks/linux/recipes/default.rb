e18917af7132185aa0f7c5a1fb9ce7117ca3a5d9
U-IIJDEV15-SRV\maru <maru@LT-E6530-911.iijdev15-srv.local>
2014-02-17 22:33:05 +0900

    2014/02/17

--- vg-chef-repo/site-cookbooks/linux/recipes/default.rb
+++ vg-chef-repo/site-cookbooks/linux/recipes/default.rb
@@ -15,20 +15,33 @@
   end
 end
 
+%w{ python-pygments }.each do |pack|
+  package pack do
+    action :install
+  end
+end
+
 execute "nettools" do
   command 'yum -y groupinstall "Networking Tools"'
   action :run
 end
 
+directory "/etc/my_env" do
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
+
 file "/etc/profile" do
   rc = Chef::Util::FileEdit.new(path)
-  my_profile = '[ -e /etc/my_profile ] && . /etc/my_profile'
+  my_profile = '[ -f /etc/my_env/profile ] && . /etc/my_env/profile'
   rc.insert_line_if_no_match( my_profile, "#{my_profile}\n")
   content rc.send(:contents).join
   action :create
 end
 
-template "/etc/my_profile" do
+template "/etc/my_env/profile" do
   source "env/my_profile.erb"
   owner "root"
   group "root"
@@ -36,12 +49,27 @@ template "/etc/my_profile" do
   action :create
 end
 
-template "/usr/share/vim/vimrc" do
-  source "env/vimrc.erb"
+template "/etc/my_env/vimrc" do
+  source "env/my_vimrc.erb"
   owner "root"
   group "root"
   mode "0644"
   action :create
 end
 
+template "/etc/my_env/lessfilter" do
+  source "env/lessfilter.erb"
+  owner "root"
+  group "root"
+  mode "0755"
+  action :create
+end
+
+template "/usr/lib/python2.6/site-packages/pygments/styles/monokai.py" do
+  source "env/monokai.py.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  action :create
+end
 
